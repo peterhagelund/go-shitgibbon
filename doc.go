dbf2000dd89fc5ca2dedb1c83fb66e48e405c2d7
Peter Hagelund <peterhagelund@mac.com>
2020-02-28 07:24:53 -0500

    update to go 1.14 and added package documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package shitgibbon provides functionality to generate Shit-Gibbon
+expressions made up of an expletive and a trochee, allowing for
+maginificent insults like Pennsylvania state senator Daylin Leach's
+"[the president is a ] fascist, loofa-faced, shit-gibbon".
+*/
+package shitgibbon
