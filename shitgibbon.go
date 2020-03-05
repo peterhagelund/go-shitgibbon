386c32591c57138575b6cb2818ab4d21225dc48e
Peter Hagelund <peterhagelund@mac.com>
2020-03-05 13:37:55 -0500

    refactored to use a generator

--- shitgibbon.go
+++ shitgibbon.go
@@ -1,6 +1,7 @@
 package shitgibbon
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -31,21 +32,57 @@ var trochees = []string{
 	"womble",
 }
 
-var rng = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+type generator struct {
+	rng     *rand.Rand
+	useDash bool
+}
 
-// Make makes a random "shitgibbon".
-func Make() string {
-	expletive := expletives[rng.Intn(len(expletives))]
-	trochee := trochees[rng.Intn(len(trochees))]
+// Generator defines a "shitgibbon" generator interface.
+type Generator interface {
+	// Returns a Boolean flag indicating whether or not the generator is using a dash between expletive and trochee.
+	UseDash() bool
+	// Sets the Boolean flag governing the use of a dash.
+	SetUseDash(flag bool)
+	// Make makes a "shitgibbon".
+	Make() string
+	// MakeAll makes all possible "shitgibbon" combinations.
+	MakeAll() []string
+}
+
+// NewGenerator creates and returns a new Generator.
+func NewGenerator() Generator {
+	return &generator{
+		rng:     rand.New(rand.NewSource(time.Now().UTC().UnixNano())),
+		useDash: false,
+	}
+}
+
+func (sg *generator) UseDash() bool {
+	return sg.useDash
+}
+
+func (sg *generator) SetUseDash(flag bool) {
+	sg.useDash = flag
+}
+
+func (sg *generator) Make() string {
+	expletive := expletives[sg.rng.Intn(len(expletives))]
+	trochee := trochees[sg.rng.Intn(len(trochees))]
+	if sg.useDash {
+		return fmt.Sprintf("%s-%s", expletive, trochee)
+	}
 	return expletive + trochee
 }
 
-// MakeAll makes all possible "shitgibbon" combinations.
-func MakeAll() []string {
+func (sg *generator) MakeAll() []string {
 	shitgibbons := make([]string, 0, len(expletives)*len(trochees))
 	for _, trochee := range trochees {
 		for _, expletive := range expletives {
-			shitgibbons = append(shitgibbons, expletive+trochee)
+			if sg.useDash {
+				shitgibbons = append(shitgibbons, fmt.Sprintf("%s-%s", expletive, trochee))
+			} else {
+				shitgibbons = append(shitgibbons, expletive+trochee)
+			}
 		}
 	}
 	return shitgibbons
--- shitgibbon_test.go
+++ shitgibbon_test.go
@@ -1,16 +1,64 @@
 package shitgibbon
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-func TestMake(t *testing.T) {
-	sg := Make()
+func TestMakeNoDash(t *testing.T) {
+	generator := NewGenerator()
+	if generator.UseDash() {
+		t.Error("Generator using dash")
+	}
+	sg := generator.Make()
 	if len(sg) == 0 {
 		t.Error("Empty shitgibbon returned")
 	}
+	if strings.IndexRune(sg, '-') >= 0 {
+		t.Error("shitgibbon returned with dash")
+	}
 }
 
-func TestMakeAll(t *testing.T) {
-	all := MakeAll()
+func TestMakeWithDash(t *testing.T) {
+	generator := NewGenerator()
+	generator.SetUseDash(true)
+	if !generator.UseDash() {
+		t.Error("Generator not using dash")
+	}
+	sg := generator.Make()
+	if len(sg) == 0 {
+		t.Error("Empty shitgibbon returned")
+	}
+	if strings.IndexRune(sg, '-') < 0 {
+		t.Error("shitgibbon returned without dash")
+	}
+}
+
+func TestMakeAllNoDash(t *testing.T) {
+	generator := NewGenerator()
+	if generator.UseDash() {
+		t.Error("Generator using dash")
+	}
+	all := generator.MakeAll()
+	if len(all) == 0 {
+		t.Error("Empty slice returned")
+	}
+	sgs := make(map[string]bool)
+	for _, sg := range all {
+		if _, ok := sgs[sg]; ok {
+			t.Errorf("Shitgibbon '%v' returned more than once", sg)
+		}
+		sgs[sg] = true
+	}
+}
+
+func TestMakeAllWithDash(t *testing.T) {
+	generator := NewGenerator()
+	generator.SetUseDash(true)
+	if !generator.UseDash() {
+		t.Error("Generator not using dash")
+	}
+	all := generator.MakeAll()
 	if len(all) == 0 {
 		t.Error("Empty slice returned")
 	}
